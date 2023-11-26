ac6c8d6208002f4e8a9f5160f259e69d5c19f213
Henrique <henriquebertolin@outlook.com>
2023-11-26 13:55:40 -0300

    Montagem da arvore e função para percorrer ela.

---
+++ arvoreExpressoes.py
@@ -0,0 +1,157 @@
+expressao = input()
+
+operadores = ['+', '-', '/', '*']
+
+class Node:
+    def __init__(self, esquerda=None, direita=None, valor=None, posicao=None):
+        self.esquerda = esquerda
+        self.direita = direita
+        self.valor = valor
+        self.posicao = posicao
+
+    def definir_esquerda(self, valor):
+        self.esquerda = valor
+
+    def definir_direita(self, valor):
+        self.direita = valor
+
+    def definir_valor(self, valor):
+        self.valor = valor
+
+    def definir_posicao(self, valor):
+        self.posicao = valor
+
+class Operadores:
+    def __init__(self, valor=None, parenteses=None, posicao = None):
+        self.valor = valor
+        self.parenteses = parenteses
+        self.posicao = posicao
+
+    def definir_valor(self, valor):
+        self.valor = valor
+
+    def definir_parenteses(self, valor):
+        self.parenteses = valor
+
+    def definir_posicao(self, valor):
+        self.posicao = valor
+
+# ENCOTRANDO A RAIZ DA ARVORE:
+def defineRoot(expressao):
+    opPresentes = []
+    for i in range (len(expressao)):
+        if expressao[i] in operadores:
+            substring = expressao[0:i]
+            operador = Operadores(valor = expressao[i], parenteses = substring.count('(') - substring.count(')'), posicao = i)
+            #print(substring)
+            opPresentes.append(operador)
+
+    raizOperador = None
+    for operador in opPresentes:
+        if raizOperador == None:
+            raizOperador = operador
+        if (operador.valor == '*' or operador.valor == '/') and raizOperador.parenteses > operador.parenteses:
+            raizOperador = operador
+        if (operador.valor == '+' or operador.valor == '-'):
+            if (raizOperador.valor == '-' or raizOperador.valor == '+'):
+                if(raizOperador.parenteses > operador.parenteses):
+                    raizOperador = operador
+            if (raizOperador.valor == '*' or raizOperador.valor == '/'):
+                if(raizOperador.parenteses >= operador.parenteses):
+                    raizOperador = operador
+
+    raiz = Node(valor=raizOperador.valor, posicao=raizOperador.posicao)
+    #print(raiz.valor)
+
+    raiz.definir_esquerda(defineSubRoot(expressao[0:raiz.posicao]))
+    raiz.definir_direita(defineSubRoot(expressao[raiz.posicao+1:]))
+    return raiz
+
+def defineSubRoot(expressao):
+    opPresentes = []
+    for i in range (len(expressao)):
+        if expressao[i] in operadores:
+            substring = expressao[0:i]
+            substringDireita = expressao[i+1:]
+            operador = Operadores(valor = expressao[i], parenteses = substring.count('(') - substring.count(')'), posicao = i)
+            opPresentes.append(operador)
+    raizOperador = None
+    nodo = None
+    if(len(opPresentes) == 0):
+        if(expressao[0] == '(' or expressao[0] == ')'):
+            expressao = expressao.replace('(', '').replace(')', '') 
+            #print (expressao[0:])
+            return expressao[0:]
+            #raizOperador = Operadores(valor = expressao[1:], posicao = 0)
+        else:
+            expressao = expressao.replace('(', '').replace(')', '')
+            #print(expressao[0:])
+            #raizOperador = Operadores(valor = expressao[0:-1], posicao = 0)
+            return expressao[0:]
+    else:    
+        for operador in opPresentes:
+            if raizOperador == None:
+                raizOperador = operador
+            if (operador.valor == '*' or operador.valor == '/') and raizOperador.parenteses > operador.parenteses:
+                raizOperador = operador
+            if (operador.valor == '+' or operador.valor == '-'):
+                if (raizOperador.valor == '-' or raizOperador.valor == '+'):
+                    if(raizOperador.parenteses > operador.parenteses):
+                        raizOperador = operador
+                if (raizOperador.valor == '*' or raizOperador.valor == '/'):
+                    if(raizOperador.parenteses >= operador.parenteses):
+                        raizOperador = operador
+    #print (raizOperador.valor)
+    nodo = Node(valor=raizOperador.valor, posicao=raizOperador.posicao)
+    nodo.definir_esquerda(defineSubRoot(substring))
+    nodo.definir_direita(defineSubRoot(substringDireita))
+
+    return nodo
+
+teste = defineRoot(expressao)
+#print(f'DIREITA DA RAIZ: {teste.direita}')
+#print(f'ESQUERDA DA RAIZ: {teste.esquerda.valor}')
+#print(f'DIREITA DA ESQUERDA DA RAIZ: {teste.esquerda.direita}')
+
+# def calcular(node):
+#     if node is None:
+#         return 0
+#     if node.valor in ['+', '-', '*', '/']:
+#         esquerda = calcular(node.esquerda)
+#         direita = calcular(node.direita)
+#         if node.valor == '+':
+#             return esquerda + direita
+#         elif node.valor == '-':
+#             return esquerda - direita
+#         elif node.valor == '*':
+#             return esquerda * direita
+#         elif node.valor == '/':
+#             if direita != 0:
+#                 return esquerda / direita
+#             else:
+#                 print("Erro: divisão por zero!")
+#                 return 0
+#     else:
+#         return int(node.valor)
+def calcular(node):
+    if isinstance(node, Node):
+        if node.valor in ['+', '-', '*', '/']:
+            esquerda = calcular(node.esquerda)
+            direita = calcular(node.direita)
+            if node.valor == '+':
+                return esquerda + direita
+            elif node.valor == '-':
+                return esquerda - direita
+            elif node.valor == '*':
+                return esquerda * direita
+            elif node.valor == '/':
+                if direita != 0:
+                    return esquerda / direita
+                else:
+                    print("Erro: divisão por zero!")
+                    return 0
+    else:
+        return float(node)
+
+#print('teste')    
+print(calcular(teste))
