4046e7579302214c541b2608dd7a7e5557a68428
vinicius <henriquebertolin@outlook.com>
2023-12-02 15:13:17 -0300

    conexao com banco

--- arvoreExpressoes.py
+++ arvoreExpressoes.py
@@ -1,3 +1,15 @@
+import psycopg2
+
+conexao = psycopg2.connect(
+    database="",
+    host="",
+    user="",
+    password="",
+    port=""
+)
+
+cursor = conexao.cursor()
+
 class Node:
     def __init__(self, esquerda=None, direita=None, valor=None, posicao=None):
         self.esquerda = esquerda
@@ -33,7 +45,7 @@ class Operadores:
         self.posicao = valor
 
 # ENCOTRANDO A RAIZ DA ARVORE:
-def defineRoot(expressao):
+def defineRoot(expressao, id):
     opPresentes = []
     for i in range (len(expressao)):
         if expressao[i] in operadores:
@@ -57,6 +69,10 @@ def defineRoot(expressao):
 
     raiz = Node(valor=raizOperador.valor, posicao=raizOperador.posicao)
     #print(raiz.valor)
+    raizVar = raiz.valor
+    comando = f"INSERT INTO expressoes (id, expressao, raiz) VALUES ({id},'{expressao}', '{raizVar}')"
+    cursor.execute(comando)
+    conexao.commit()
 
     raiz.definir_esquerda(defineSubRoot(expressao[0:raiz.posicao]))
     raiz.definir_direita(defineSubRoot(expressao[raiz.posicao+1:]))
@@ -118,11 +134,55 @@ def calcular(node):
                     return 0
     else:
         return float(node)
+while (True):
+    print("Você deseja realizar uma operação, fazer uma consulta ou sair? \n1 - Operação \n2 - Consulta \n3- Sair")
+    decisao = int(input())
+    if (decisao == 1):
+        print("Digite uma expressão: ")
+        expressao = input()
+        if(expressao.count('(') != expressao.count(')')):
+            print("Erro: parênteses desbalanceados")
+        else:
+            comando_max_id = "SELECT MAX(id) FROM expressoes"
+            cursor.execute(comando_max_id)
+            maior_id = cursor.fetchone()[0]
+            if (maior_id is None): 
+                id = 1
+            else:
+                id = maior_id + 1
+            operadores = ['+', '-', '/', '*']
+            calcularExpressao = defineRoot(expressao, id)
 
-expressao = input()
-if(expressao.count('(') != expressao.count(')')):
-    print("Erro: parênteses desbalanceados")
-else:
-    operadores = ['+', '-', '/', '*']
-    calcularExpressao = defineRoot(expressao)
-    print(calcular(calcularExpressao))
+            resultado = calcular(calcularExpressao)
+            print(calcular(calcularExpressao))
+            comando = f"update expressoes set resultado = {resultado} where id = {id}"
+            cursor.execute(comando)
+            conexao.commit()
+
+
+    elif (decisao == 2):
+        print("Você deseja listar todas as operações ou buscar pelo ID? \n1 - Listar todas \n2 - ID")
+        decisaoConsulta = int(input())
+        if (decisaoConsulta == 1):
+            comando = f"select * from expressoes"
+            cursor.execute(comando)
+            resultados = cursor.fetchall()
+            for resultado in resultados:
+                id, expressao, resultado_valor, raiz = resultado
+                resultado_float = float(resultado_valor)
+                print(f"ID: {id}, Expressao: {expressao}, Resultado: {resultado_float}, Raiz: {raiz}")
+        else:
+            print("Digite o ID da operação que você deseja buscar: ")
+            id = int(input())
+            comando = f"select * from expressoes where id = {id}"
+            cursor.execute(comando)
+            resultados = cursor.fetchall()
+            for resultado in resultados:
+                id, expressao, resultado_valor, raiz = resultado
+                resultado_float = float(resultado_valor)
+                print(f"ID: {id}, Expressao: {expressao}, Resultado: {resultado_float}, Raiz: {raiz}")
+    else:
+        break
+
+cursor.close()
+conexao.close()
